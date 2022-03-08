a0e7e27bb3b615a431dc0c9d6f62a4ee82b31e61
SudhanRoshan <19tucs233@skct.edu.in>
2022-03-08 11:19:45 +0530

    Updated Code

---
+++ api.py
@@ -0,0 +1,216 @@
+from flask import Flask
+from flask_restx import Api, Resource, fields
+from werkzeug.middleware.proxy_fix import ProxyFix
+from datetime import date
+import time
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "Roshan02"
+app.config["MYSQL_DB"] = "flaskapp"
+mysql = MySQL(app)
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='1.0', title='TodoMVC API',
+          description='A simple TodoMVC API',
+          )
+
+ns = api.namespace('todos', description='TODO operations')
+
+todo = api.model('Todo', {
+    'id': fields.Integer(readonly=True, description='The task unique identifier'),
+    'task': fields.String(required=True, description='The task details'),
+
+    # Adding two new fields to each task (due_date and status)
+
+    'due_date': fields.String(required=True, description='The task due'),
+    'status': fields.String(required=True, description='The status of the task'),
+})
+
+
+class TodoDAO(object):
+    def __init__(self):
+        self.counter = 0
+        self.todos = []
+
+    def get(self, id):
+        for todo in self.todos:
+            if todo['id'] == id:
+                return todo
+        api.abort(404, "Todo {} doesn't exist".format(id))
+
+    def getTodos(self, due_date):
+        result = []
+        for todo in self.todos:
+            if todo["due_date"] == due_date and todo["status"] != "Finished":
+                result.append(todo)
+        return result
+
+    def create(self, data):
+        todo = data
+        todo['id'] = self.counter = self.counter + 1
+        self.todos.append(todo)
+        return todo
+
+    def update(self, id, data):
+        todo = self.get(id)
+        todo.update(data)
+        return todo
+
+    def delete(self, id):
+        todo = self.get(id)
+        self.todos.remove(todo)
+
+
+DAO = TodoDAO()
+
+
+@ns.route("/")
+class TodoList(Resource):
+    '''Shows a list of all todos, and lets you POST to add new tasks'''
+    @ns.doc('list_todos')
+    @ns.marshal_list_with(todo)
+    def get(self):
+        '''List all tasks'''
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM todos")
+        x = cur.fetchall()
+        result = []
+        for i in x:
+            result.append({"id": i[0], "task": i[1],
+                          "due_date": i[2], "status": i[3]})
+        return result
+
+    @ns.doc('create_todo')
+    @ns.expect(todo)
+    @ns.marshal_with(todo, code=201)
+    def post(self):
+        '''Create a new task'''
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO todos(task,due_date,status,id) VALUES(%s,%s,%s,%s)", (DAO.create(
+            api.payload)["task"], DAO.create(api.payload)["due_date"], DAO.create(api.payload)["status"], DAO.create(api.payload)["id"]))
+        mysql.connection.commit()
+        cur.close()
+        return DAO.create(api.payload), 201
+
+# Additional endpoint (i.e) "GET/finished"
+
+
+@ns.route("/finished")
+class getFinished(Resource):
+    '''Shows a list of all todos that are finished'''
+    @ns.doc('list_finished_todos')
+    @ns.marshal_list_with(todo)
+    def get(self):
+        '''List all the finished tasks'''
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM todos")
+        y = cur.fetchall()
+        result = []
+        for i in y:
+            if i[3] == "Finished":
+                result.append({"id": i[0], "task": i[1],
+                               "due_date": i[2], "status": i[3]})
+        return result
+
+
+# Additional endpoint (i.e) "GET/overdue"
+
+@ns.route("/overdue")
+class getOverdue(Resource):
+    '''Shows a list of all todos that are overdue'''
+    @ns.doc('list_overdue_todos')
+    @ns.marshal_list_with(todo)
+    def get(self):
+        '''List all the overdue tasks'''
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM todos")
+        z = cur.fetchall()
+        print(z)
+        result = []
+
+        for i in z:
+            dueDate = i[2]
+            formattedDueDate = time.strptime(dueDate, "%Y-%m-%d")
+            currentDate = date.today()
+            strCurrentDate = str(currentDate)
+            formattedCurrentDate = time.strptime(strCurrentDate, "%Y-%m-%d")
+            if formattedDueDate < formattedCurrentDate:
+                result.append({"id": i[0], "task": i[1],
+                               "due_date": i[2], "status": i[3]})
+        return []
+
+
+@ns.route('/<int:id>')
+@ns.response(404, 'Todo not found')
+@ns.param('id', 'The task identifier')
+class Todo(Resource):
+    '''Show a single todo item and lets you delete them'''
+    @ns.doc('get_todo')
+    @ns.marshal_with(todo)
+    def get(self, id):
+        '''Fetch a given resource'''
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM todos")
+        a = cur.fetchall()
+        result = []
+        for i in a:
+            if i[0] == id:
+                result.append({"id": i[0], "task": i[1],
+                               "due_date": i[2], "status": i[3]})
+        return result
+
+    @ns.doc('delete_todo')
+    @ns.response(204, 'Todo deleted')
+    def delete(self, id):
+        '''Delete a task given its identifier'''
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM todos WHERE id = {}".format(id))
+        mysql.connection.commit()
+        return "DELETED"
+
+# We can change the status of task using below method if given an identifier
+
+    @ns.expect(todo)
+    @ns.marshal_with(todo)
+    def put(self, id):
+        '''Update a task given its identifier'''
+        cur = mysql.connection.cursor()
+
+        cur.execute("UPDATE todos SET task = %s, due_date = %s,status=%s WHERE id ={}".format(id),
+                    (DAO.create(
+                        api.payload)["task"], DAO.create(api.payload)["due_date"], DAO.create(api.payload)["status"], ))
+        mysql.connection.commit()
+        return "UPDATED"
+        return DAO.update(id, api.payload)
+
+# Additional endpoint (i.e) "GET/due?due_date=yyyy-mm-dd"
+
+
+@ns.route('/<string:due_date>')
+@ns.response(404, 'No Dues')
+@ns.param('due_date', 'Due date for the task')
+class getTodosForGivenDate(Resource):
+    '''Shows dues with given date'''
+    @ns.doc('get_dues_with_given_date')
+    @ns.marshal_with(todo)
+    def get(self, due_date):
+        '''Fetch dues of the day'''
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM todos")
+        b = cur.fetchall()
+        print("b===============================>", b)
+        result = []
+        for i in b:
+            if i[2] == due_date and i[3] != "Finished":
+                result.append({"id": i[0], "task": i[1],
+                               "due_date": i[2], "status": i[3]})
+        return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ part2.py
@@ -0,0 +1,118 @@
+# PART II
+
+# Reading the csv file
+
+from csv import reader as csv_reader
+
+# Headers in the given csv file are Name of Subjects
+headers = True
+
+
+# Taking the marks input as Array of Dictionaries
+
+# Declaring outer array
+marks = []
+
+try:
+    with open("Marks Report.csv", 'r') as file:
+        fileReader = csv_reader(file)
+        if headers:
+            columns = next(fileReader)
+        for row in fileReader:
+
+            # Declaring inner dictionaries
+            row_data = {}
+
+            for i in range(len(row)):
+                if headers:
+                    row_key = columns[i].lower()
+                else:
+                    row_key = i
+                row_data[row_key] = row[i]
+            marks.append(row_data)
+except Exception as e:
+    print(repr(e))
+
+# Object Oriented Programming Approach
+
+
+class Students:
+    def _init_(self, details):
+        self.student_details = details
+
+    # Finding topper in each subject
+
+    def topperEachSubject(self, subjectName, student_details):
+
+        # Below code also handle students with same marks
+        # time -> O(n) | space -> O(1)best case ,O(n)worst case
+
+        largest = [0]
+        for i in range(1, len(student_details)):
+
+            if(int(student_details[largest[0]][subjectName]) < int(student_details[i][subjectName])):
+                largest = [i]
+
+            elif(int(student_details[largest[0]][subjectName]) == int(student_details[i][subjectName])):
+                largest.append(i)
+
+        # Shows the topper in each subject
+
+        print("Topper in {} is".format(subjectName), end=" ") if len(
+            largest) == 1 else print("Toppers in {} are".format(subjectName), end=" ")
+
+        for Marks in largest:
+            print("{}".format(student_details[Marks]['name']), end="  ")
+        print()
+
+    def getValue(self, index, subjectName, student_details):
+        return int(student_details[index][subjectName])
+
+    # Calculating total marks of each student
+
+    def calculateTotal(self, student_details):
+        # time->O(n) | space O(1) bestcase
+        for index in range(0, len(student_details)):
+            student_details[index]['total'] = self.getValue(index, 'maths', student_details)+self.getValue(index, 'biology', student_details)+self.getValue(
+                index, 'english', student_details)+self.getValue(index, 'physics', student_details)+self.getValue(index, 'chemistry', student_details)+self.getValue(index, 'hindi', student_details)
+
+   # Finding best three students in the class
+
+    def topThreeStudents(self, student_details):
+        # time->O(3n) | space O(1) bestcase
+        self.calculateTotal(student_details)
+        largest = 0
+        for i in range(1, len(student_details)):
+            if(int(student_details[largest]['total']) < int(student_details[i]['total'])):
+                largest = i
+        student_details[largest]['total'] = -1
+
+        secondlarge = 0
+        for i in range(1, len(student_details)):
+            if(int(student_details[i]['total']) != -1 and int(student_details[secondlarge]['total']) < int(student_details[i]['total'])):
+                secondlarge = i
+
+        student_details[secondlarge]['total'] = -1
+
+        thirdlarge = 0
+        for i in range(1, len(student_details)):
+            if(int(student_details[i]['total']) != -1 and int(student_details[thirdlarge]['total']) < int(student_details[i]['total'])):
+                thirdlarge = i
+
+        # Shows the best three students in the class
+
+        print("Best students in the class are ({}, {} and {}).".format(
+            student_details[largest]['name'], student_details[secondlarge]['name'], student_details[thirdlarge]['name']))
+        print()
+
+
+# Declaring Object for Students class
+
+obj = Students()
+obj.topperEachSubject("maths", marks)
+obj.topperEachSubject("biology", marks)
+obj.topperEachSubject("english", marks)
+obj.topperEachSubject("physics", marks)
+obj.topperEachSubject("chemistry", marks)
+obj.topperEachSubject("hindi", marks)
+obj.topThreeStudents(marks)
